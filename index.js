1408bed705fa6fa23394243d9efb3405044a8a95
Slava Bereza <mail@betula.co>
2022-08-05 14:13:43 +0700

    feat: interface

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const 
+  event = () => {},
+  fire = () => {},
+  map = () => {},
+  filter = () => {},
+  readonly = () => {},
+  listen = () => {};
+
+listen.once = () => {};
+
+module.exports = {
+  event,
+  fire,
+  map,
+  filter,
+  readonly,
+  listen
+}
