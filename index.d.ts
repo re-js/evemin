6c885644aecfe4d2c9bad8ea667a3c8670118314
Slava Bereza <mail@betula.co>
2022-08-12 20:51:22 +0700

    chore: export type of event

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-type Event<P> = ((data: P) => void) & [((data: P) => void)[]];
+export type Event<P> = ((data: P) => void) & [((data: P) => void)[]];
 
 export declare const event: <P = void>() => Event<P>;
 export declare const listen: <P>(event: Event<P>, listener: (data: P) => void) => (() => void);
