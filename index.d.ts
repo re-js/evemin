999830547d1ce8012514f39b7656bedb24be71da
Slava Bereza <mail@betula.co>
2022-08-12 13:07:24 +0700

    feat: typescript types

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+
+type Event<P> = ((data: P) => void) & [((data: P) => void)[]];
+
+export declare const event: <P = void>() => Event<P>;
+export declare const listen: <P>(event: Event<P>, listener: (data: P) => void) => (() => void);
